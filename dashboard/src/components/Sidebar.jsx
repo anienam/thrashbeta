c2db6b6082e10c43ab9b73c3d6acb8ae67cbd625
Abasiomofon Udoh <omofon.u@gmail.com>
2026-01-31 16:38:01 +0100

    feat: Sidebar logo styling

--- dashboard/src/components/Sidebar.jsx
+++ dashboard/src/components/Sidebar.jsx
@@ -1,3 +1,5 @@
+import logo from "../assets/logo.svg";
+
 export default function Sidebar() {
   const menuItems = [
     { label: "Dashboard", active: true },
@@ -7,25 +9,27 @@ export default function Sidebar() {
   ];
 
   return (
-    <aside className="w-60 h-screen bg-white border-r border-neutra-300 flex flex-col justify-between items-center p-6 sticky top-0">
-      <div className="flex items-center justify-center w-full px-5 py-4 border border-neutra-300">
-        <img
-          src="../assets/logo.svg"
-          alt="Trashbeta Logo"
-          className="w-fit h-fit overflow-hidden"
-        />
+    <aside className="w-60 h-screen bg-white border-neutra-300 flex flex-col justify-between items-start sticky top-0">
+      <div>
+        <div className="flex items-center justify-center w-full h-18 px-5 py-4 border-b border-neutra-300">
+          <img
+            src={logo}
+            alt="Trashbeta Logo"
+            className="w-fit h-fit overflow-hidden"
+          />
+        </div>
+        <nav className="flex-1 space-y-2">
+          {menuItems.map((item) => (
+            <a
+              key={item.label}
+              href="#"
+              className={`block px-4 py-3 rounded-xl transition-colors ${item.active ? "bg-primary-100 text-primary-500 font-semibold rounded-full" : "text-neutra-700 hover:bg-neutra-50"}`}
+            >
+              {item.label}
+            </a>
+          ))}
+        </nav>
       </div>
-      <nav className="flex-1 space-y-2">
-        {menuItems.map((item) => (
-          <a
-            key={item.label}
-            href="#"
-            className={`block px-4 py-3 rounded-xl transition-colors ${item.active ? "bg-primary-100 text-primary-500 font-semibold rounded-full" : "text-neutra-700 hover:bg-neutra-50"}`}
-          >
-            {item.label}
-          </a>
-        ))}
-      </nav>
       <div className="mt-auto pt-6 border-t border-gray-100">
         <div className="flex items-center gap-3">
           <img
